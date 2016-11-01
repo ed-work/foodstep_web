c4da3b8cccb97fce9686aa6d5ed952096f0be813
dayatz <dayat.py@gmail.com>
2016-11-01 20:28:57 +0800

    user's recipe

--- food_step/apps/core/views.py
+++ food_step/apps/core/views.py
@@ -1,9 +1,20 @@
-from rest_framework import viewsets
 from django.contrib.auth.models import User
-
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from apps.recipe.models import Recipe
+from apps.recipe.serializers import RecipeSerializer
 from .serializers import UserSerilizer
 
 
 class UserViewset(viewsets.ModelViewSet):
     serializer_class = UserSerilizer
     queryset = User.objects.all()
+
+    @detail_route()
+    def recipes(self, request, *args, **kwargs):
+        recipes = Recipe.objects.select_related('user')\
+            .filter(user_id=self.kwargs.get('pk'))
+        serializer = RecipeSerializer(
+            recipes, many=True, context={'request': request})
+        return Response(serializer.data)
