9148377891044f7b770ab6ab56a3ede047b3dd2d
dayatz <dayat.py@gmail.com>
2016-11-02 13:59:15 +0800

    favorites recipe

--- food_step/apps/recipe/views.py
+++ food_step/apps/recipe/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import list_route
 
 from .models import Recipe, Step, Ingredient
 from .serializers import RecipeSerializer, StepSerializer, IngredientSerializer
@@ -7,6 +9,12 @@ from .serializers import RecipeSerializer, StepSerializer, IngredientSerializer
 class RecipeViewset(viewsets.ModelViewSet):
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
+
+    @list_route()
+    def favorites(self, request, *args, **kwargs):
+        qs = self.queryset.filter(user=request.user)
+        serializer = self.serializer_class(qs, many=True)
+        return Response(serializer.data)
 
 
 class StepViewset(viewsets.ModelViewSet):
