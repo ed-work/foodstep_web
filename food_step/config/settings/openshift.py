5648190f0057d340f0050c39e0cd8909da7ce5e6
dayatz <dayat.py@gmail.com>
2016-11-16 14:55:45 +0800

    setting for openshift

---
+++ food_step/config/settings/openshift.py
@@ -0,0 +1,40 @@
+import os
+from .base import *
+
+DEBUG = False
+
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('OPENSHIFT_APP_NAME'),
+        'USER': os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
+        'PASSWORD': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
+        'HOST': os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST'),
+        'PORT': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT')
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django.log'),
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+    },
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '../wsgi/media')
